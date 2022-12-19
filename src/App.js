7701d2f9c200f711c4164e75d876b531d3e4f3d0
sweta <swetabisht4b@outlook.com>
2022-12-19 18:59:11 +0530

    added emoji

--- src/App.js
+++ src/App.js
@@ -1,54 +1,61 @@
-
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
-
-var emojiDictionary={
-  "smile": "😊",
-  "sad": "😔",
-  "happy": "😀",
-  "anger": "😠",
-  "confused": "😕",
-  "love": "❤️"
+var emojiDictionary = {
+  smile: "😊",
+  sad: "😔",
+  happy: "😀",
+  anger: "😠",
+  confused: "😕",
+  love: "❤️",
+  panda: "🐼 ",
+  cake: "🎂",
+  drink: "🍹",
+  run: "🏃"
 };
- 
- var emojiWeKnow=Object.keys(emojiDictionary);
+
+var emojiWeKnow = Object.keys(emojiDictionary);
 
 export default function App() {
+  const [meaning, setMeaning] = useState("");
 
-const [meaning, setMeaning] = useState("");
+  function inputChangeHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
 
-function inputChangeHandler(event){
+    if (meaning === undefined) {
+      meaning = "sorry, its not in our data";
+    }
+    setMeaning(meaning);
+  }
 
-var userInput=event.target.value;
-var meaning=emojiDictionary[userInput];
-
-if(meaning===undefined){
-  meaning= "sorry, its not in our data";
-}
-setMeaning(meaning);
-}
-
-function emojiClickHandler(emoji){
-  var meaning=emojiDictionary[emoji];
-  setMeaning(meaning);
-}
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
 
   return (
     <div className="App">
       <h1>Inside Out</h1>
-      <input placeholder="find the emoji by name" onChange={inputChangeHandler}></input>
-      <h2>{ meaning }</h2>
+      <input
+        placeholder="find the emoji by name"
+        onChange={inputChangeHandler}
+      ></input>
+      <h2>{meaning}</h2>
 
-     <h3>emoji we know</h3>
+      <h3>emoji we know</h3>
 
-     {emojiWeKnow.map(function (emoji){
-       return (
-         <span onClick={()=>emojiClickHandler(emoji)} style={{ padding: "0.5rem", cursor: "pointer"  }}> { emoji }</span>
-       );
-     })
-     }
-
+      {emojiWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ padding: "0.5rem", cursor: "pointer" }}
+          >
+            {" "}
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
